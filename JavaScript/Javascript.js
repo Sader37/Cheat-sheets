14f2f08f29f7907645d64f67b548f9c2b4e50d91
Aszura <40485301+Sader37@users.noreply.github.com>
2019-08-03 23:06:36 +0300

    Update Javascript.js

--- JavaScript/Javascript.js
+++ JavaScript/Javascript.js
@@ -169,3 +169,39 @@ continue loop1, break loop1; // used to whether continue loop1 or break it
 import { foo as bar } from "./baz";
 import { default as bar } from "./baz";
 import * as foo from "foo"; // foo.bar(), foo.baz()
+// ----- Regex
+//Flags
+i - regex isn’t case sensitive, g - all matches, m - multiline mode.
+str.search(reg) — searches for first match or return -1.
+str.match(reg) — without g: returns result, result[0] - first match, result[n] - other matches(if reg grouped by parentheses), result.index - position of match, result.input - str;
+— with g: returns array of matches, null if there’s no matches.
+str.split(reg|substring, limit) — breaks str into an array using reg, can limit up amount of items.
+str.replace(reg, str|func) — replaces all the matches of reg, argument can use: $n - inserts n, $& - inserts all the matches, $` - inserts str part before of match, $’ - inserts str part after the match, func cat take the following ars: func(str, [,p1,p2,..], offset, s) — str - found match, [,p1,p2…] - parentheses content(if they’re present), offset - position where match was found, s - initial string.
+regex.test(str) — returns true/false if there’s a match
+regex.exec(str) — searches for matches: regex.lastIndex shows where the search is carrying out.
+Classes and special symbol
+\d - digit, symbol from 0 to 9, \D - not a digit,
+\s - space/tab|str-break, \S - not a space
+\w - word, \w - not a word,
+\b - border of word, \B - not a border
+Spaces are common symbols.
+“.” - any symbol but a break.
+To escape symbol as regular backslash should be used
+Sets and ranges.
+[bac] - set means that any of a, b or c can present.
+[0-5] - range means that any number between 0 and 5 inclusively and present.
+[\wа-яё] - range finds any english and russian letter.
+Exclusion ranges: [^...]
+Symbols inside of sets and ranges are not necessary to escape.
+Quantifiers 
+{ n } — exact quantity; { n, n + m } — quantity from-to; { n, } — quantity from n to infinity;
++ — one or more, ? —  zero or one, * — zero or more
+Greedy search is the regular one, to switch to lazy search use ? after quantifiers: +?, *?, ??
+( ) — parentheses are used to group up elements.
+for instance, (( )\s*( )) — will store all (3) parentheses in result array, starting from exterior ones.
+To use grouping without storing data inside of parentheses use “?:” g.e.:  (?: )
+\n — used to repeat parentheses in regex, | — alternation.
+^ — at the regex beginning is the text beginning,
+^ — at the regex with m flag is the string beginning,
+$ — at the end of regex defines text ending, 
+$ — at the end of regex with m flag is the string ending.
