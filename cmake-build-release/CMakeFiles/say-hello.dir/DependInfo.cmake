1ae29ed11301aeaad19dbc68489fb392c8314d81
Aszura <saderdan9@gmail.com>
2019-09-30 13:36:12 +0300

    dummy

---
+++ cmake-build-release/CMakeFiles/say-hello.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/programming/CPP_Cheats/hello.cpp" "C:/programming/CPP_Cheats/cmake-build-release/CMakeFiles/say-hello.dir/hello.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "say_hello_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Program Files/MySQL/MySQL Connector C++ 8.0/include"
+  "C:/Program Files/MySQL/MySQL Connector C++ 8.0/include/mysqlx"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-release/CMakeFiles/say-hello.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/say-hello.dir/hello.cpp.obj"
+  "libsay-hello.dll"
+  "libsay-hello.dll.a"
+  "libsay-hello.dll.manifest"
+  "libsay-hello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/say-hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
