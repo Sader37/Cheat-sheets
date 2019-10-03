7722a3e24e6ac97f0a892bd9ad27977f9f54392f
Aszura <saderdan9@gmail.com>
2019-10-03 16:48:24 +0300

    changes

--- main.cpp
+++ main.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
 #include <chrono>
-#include <unistd.h>
 #include <cmath>
-#include <iomanip>
-#include <type_traits>
-
-struct D{};
+//#include <xdevapi.h>
 
 using namespace std;
+//using namespace mysqlx;
+
+//template <typename H, typename ...T>
+//void fc(H h , T...) {
+//    cout << h << endl;
+//    fc(T...);
+//
+//}
+
+//void accept_connection(Session *sess) {
+//    cout << "Session accepted, creating collection..." << endl;
+//    Schema sch = sess->getSchema("test");
+//    Collection coll = sch.createCollection("c1", true);
+//}
+
 int main() {
-    bool a_ = is_class<D>();
-    bool a = is_arithmetic<int>();
-    cout << a_ << endl;
-    cout << a << endl;
-//    bool a = is_arithmetic<int>(1);
+    void *ptr = malloc(sizeof(uint32_t));
+	cout << "Hello world!" << endl;
+    call_equation();
+    //    try {
+//        const char *url = "mysqlx://root@127.0.0.1";
+//        cout << "Creating session on " << url
+//             << " ..." << endl;
+//        Session sess(url);
+//
+//        cout << "Done!" << endl;
+//        return 0;
+//    } catch (...) {
+//        cerr << "Unexpected error.";
+//    }
+//
     return 0;
 }
